72ea62bf9003fc66ba4e03a3c8f43f0044c665b0
Gustavo Souza <gtssouza94@gmail.com>
2020-07-04 20:39:57 -0300

    PostagemAdapter com post.size funcionando, MainActivity com ArrayList funcionando

--- app/src/main/java/com/example/cardview/activity/MainActivity.java
+++ app/src/main/java/com/example/cardview/activity/MainActivity.java
@@ -8,10 +8,15 @@ import android.os.Bundle;
 
 import com.example.cardview.R;
 import com.example.cardview.adapter.PostagemAdapter;
+import com.example.cardview.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerPostagem;
+    private List<Post> listaPost = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,22 @@ public class MainActivity extends AppCompatActivity {
         recyclerPostagem.setLayoutManager(layoutManager);
 
         //Define Adapter
-        PostagemAdapter adapter = new PostagemAdapter();
+        this.prepararPostagens();
+        PostagemAdapter adapter = new PostagemAdapter(listaPost);
         recyclerPostagem.setAdapter(adapter);
+    }
+
+    public void prepararPostagens(){
+        Post p = new Post("Albert Einstein","#tbt Viagem legal!",R.drawable.imagem1);
+        this.listaPost.add(p);
+
+        p = new Post("Neils Bohr","#tbt legal!",R.drawable.imagem2);
+        this.listaPost.add(p);
+
+        p = new Post("Isaac Newton","#tbt Super!",R.drawable.imagem3);
+        this.listaPost.add(p);
+
+        p = new Post("Julio Verne",";)",R.drawable.imagem4);
+        this.listaPost.add(p);
     }
 }
--- app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
+++ app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.cardview.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,32 +9,50 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cardview.R;
+import com.example.cardview.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> {
+
+    private List<Post> post;
+    public PostagemAdapter(List<Post> p) {
+        this.post = p;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.postagem_layout,parent,false);
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        Post postagem = post.get(position);
+        holder.textNome.setText(postagem.getNome());
+        holder.textPostagem.setText(postagem.getPostagem());
+        holder.imagePostagem.setImageResource(postagem.getImagem());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return post.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
-        private TextView nome;
-        private TextView postagem;
-        private ImageView imagem;
+        private TextView textNome;
+        private TextView textPostagem;
+        private ImageView imagePostagem;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            textNome = itemView.findViewById(R.id.textNome);
+            textPostagem = itemView.findViewById(R.id.textPostagem);
+            imagePostagem = itemView.findViewById(R.id.imagePostagem);
         }
     }
 
